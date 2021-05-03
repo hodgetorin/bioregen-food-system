59c13f16c1e4cdb3559d803619b40959b75ebd61
Torin <74333325+hodgetorin@users.noreply.github.com>
2021-05-02 20:34:03 -0700

    Update combined-final.py

--- combined-final.py
+++ combined-final.py
@@ -9,23 +9,26 @@ n_crew = 8
 
 ## Initializing Plants (This version: Sunflowers only) ##
 
-# harvest indexes: % plant biomass that is edible
+# H --> Days until maturation/days until harvest
+H_su = 100
+
+# HI --> harvest indexes: % of plant biomass that is edible
 HI_su = 0.4
 
-# Growing area
-su_GA = 1
-su_GD = 6 *10000 
+# GA --> Growing area; GD --> Growing density
+GA_su = 1 # m^2
+GD_su = 60000 # g/m^2
 
 p_ed_su = np.zeros(n_days)
 p_ined_su = np.zeros(n_days)
 
-p_su = np.zeros(n_days)
+p_su = np.zeros(n_days) 
 p_ined = np.zeros(n_days)
 
-p_su[0] = 0 # biomass of sunflowers [g]
-p_su_kcal = 6.25 #sunflower seeds calories [kcal/g]
+p_su[0] = 0 # biomass of entire sunflower plants [g]
+p_su_kcal = 6.25 # sunflower seeds calories [kcal/g]
 
-H_su = 100 #days til maturation, ready to harvest
+
 
 ## Initializing Mushrooms ##
 m_ed = np.zeros(n_days)
@@ -36,7 +39,7 @@ min_sub_to_start = 1000 # placeholder value. need 1000g inedible mass to start a
 
 for i in range(n_days):
     if i % H_su == 0: # Harvest sunflowers every 100 days
-        p_su[i] = su_GA * su_GD # Growing area * growing density 
+        p_su[i] = GA_su * GD_su # Growing area * growing density 
         
     p_ed_su[i] = p_su[i] * HI_su
     p_ined_su[i] = (1-HI_su)*p_su[i] # inedible sunflower mass produced per day 
@@ -50,10 +53,14 @@ for i in range(n_days):
 
 
 # time array for plotting:
-t = np.linspace(0,n_days,n_days+1)
+t = np.linspace(1,n_days,n_days)
+
+# convert from g to kg:
+for i in range(n_days):
+    p_ed_su[i] = p_ed_su[i]/1000
+    m_ed[i] = m_ed[i]/1000
     
-#plt.plot(t, )
-
-
-
-
+plt.plot(t, p_ed_su, "b-", t, m_ed, "r--")
+plt.title("Food produced over time")
+plt.xlabel("time (days)"); plt.ylabel("food produced (kg)")
+plt.legend(["Sunflowers", "Mushrooms"], loc="best")
